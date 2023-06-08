204d85d4c8c7bccec430840663b83f07ac17d327
rejaul360 <rejaraju2k22@gmail.com>
2023-06-09 03:41:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+require('dotenv').config()
+// const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+const port = process.env.PORT || 5000;
+
+
+// middleware
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("", cors(corsConfig))
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arwkaoj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    // const usersCollection = client.db("summerSC").collection("users");
+    const classCollection = client.db("summerSC").collection("class");
+    const instractorCollection = client.db("summerSC").collection("instractor");
+    const usersCollection = client.db("summerSC").collection("users");
+
+
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        console.log(user);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+        res.send({ token })
+      })
+
+    //Users related apis 
+    app.get('/users', async (req, res) => {
+        const result = await usersCollection.find().toArray()
+        res.send(result)
+      })
+
+    app.post('/users', async (req, res) => {
+        const users = req.body
+        const query = { email: users.email }
+        const existingUser = await usersCollection.findOne(query)
+        if (existingUser) {
+          return res.send({ message: 'already exists account' })
+        }
+        const result = await usersCollection.insertOne(users)
+        res.send(result)
+      })
+
+      app.patch('/users/admin/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+        const result = await usersCollection.updateOne(query, updateDoc);
+        res.send(result)
+      })
+      app.patch('/users/instractor/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const updateDoc = {
+          $set: {
+            role: 'instractor'
+          },
+        };
+        const result = await usersCollection.updateOne(query, updateDoc);
+        res.send(result)
+      })
+
+    //class related apis 
+    app.get('/class', async (req, res) => {
+        const result = await classCollection.find().toArray();
+        res.send(result);
+      })
+
+
+          //instractor related apis 
+      app.get('/instractor', async (req, res) => {
+        const result = await instractorCollection.find().toArray();
+        res.send(result);
+      })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Summer camp on School')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Summer camp on running this year ${port}`);
+  })
